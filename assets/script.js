690bfe661fde5f94ae7484d1edaf4a0118207fbf
morganhel <138800018+morganhel@users.noreply.github.com>
2023-08-24 17:02:05 +0200

    Ajout des bullets points

--- assets/script.js
+++ assets/script.js
@@ -17,14 +17,32 @@ const slides = [
 	}
 ]
 
-let left = document.getElementById("arrow_left");
-
+// Récupération elt flèche gauche
+const left = document.getElementById("arrow_left");
+// Ajout Event Listener
 left.addEventListener("click", () => {
     console.log("Vous avez cliqué sur le bouton gauche")
+	for (var i = 0; i < slides.length; i++){
+
+	}
 });
 
-let right = document.getElementById("arrow_right");
-
+// Flèche droite
+const right = document.getElementById("arrow_right");
 right.addEventListener("click", () => {
     console.log("Vous avez cliqué sur le bouton droit")
 });
+
+// Ajout des bullets point
+for (let i = 0; i < slides.length; i++) { //Un point pour chaque diapo
+	const dot = document.createElement("div"); //Création d'un elt div 
+	dot.classList.add("dot"); //Ajout de la class="dot" à l'elt
+	const dots = document.getElementById("dots"); //Récupération de la div de class="dots"
+	dots.appendChild(dot); //Ajout de l'elt dans la div de class="dots"
+
+	if (i === 0){
+		dot.classList.add("dot_selected");  //point signalant la diapo en cour
+	}
+}
+
+
